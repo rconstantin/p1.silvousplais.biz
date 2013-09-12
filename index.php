033de7bea913c75c73f74e829f8131b18b46e1b5
Robbie Constantin <robbieconstantin@gmail.com>
2013-09-12 15:59:23 -0400

    added content and styling per requirements

--- index.php
+++ index.php
@@ -3,11 +3,36 @@
 <head>
 <title>Robbie's Bio</title>
 <meta charset="UTF-8">
+<link rel="stylesheet" href="myStyle1.css">
 </head>
 <body>
+<div id="header_container">
+	<div id="header">
+		CSCI E-15 Dynamic Web Applications: Project 1
+	</div>
+</div>		
 
-<?php
-	echo "Welcome to Project 1!";
-?>
+<h1>
+<?php 
+	echo "Robbie Constantin";
+?>	
+</h1>
+
+<h1> Bio </h1>
+
+<div class="ex"> I spend some 20+ years in reseach and development in the telecom industry. 
+In my last company, I worked on UMTS/Wireless products to deliver 2G/3G (mobile) multi-media services.
+My current and most challenging position is stay at home dad for 3 years old twin boys: Enzo & Luca.
+</div>
+
+<div class="ex1">  <strong> Code Editor: </strong> Sublime Text 2
+</div>
+
+<div id="footer_container">
+	<div id="footer"> 
+		Harvard Extension School
+    </div> 
+</div>
+
 </body>
 </html>
